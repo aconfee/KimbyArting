7229b21cf330f5bb13bc707b033e57a84218322e
System Administrator <root@Adams-MacBook-Air.local>
2016-08-23 23:56:26 -0500

    Social links added.

--- client/KimbyArting/components/cover-page/cover-page.component.ts
+++ client/KimbyArting/components/cover-page/cover-page.component.ts
@@ -2,7 +2,11 @@ import { Component, AfterViewInit } from '@angular/core';
 
 import '../../../../public/images/cover/lightBlueBackground.png';
 import '../../../../public/images/cover/egyptGirlCover.png';
-import '../../../../public/images/cover/logo.png';
+import '../../../../public/images/cover/artstationIcon.png';
+import '../../../../public/images/cover/facebookIcon.png';
+import '../../../../public/images/cover/instagramIcon.png';
+import '../../../../public/images/cover/tumblrIcon.png';
+import '../../../../public/images/cover/twitterIcon.png';
 
 @Component({
   selector: 'cover-page',
@@ -13,10 +17,12 @@ import '../../../../public/images/cover/logo.png';
 export class CoverPageComponent implements AfterViewInit{
   enter: boolean;
   exit: boolean;
+  showSocialLinks: boolean;
 
   constructor(){
     this.enter = false;
     this.exit = false;
+    this.showSocialLinks = false;
   }
 
   ngAfterViewInit() {
