41e2196e6129037f3d8264140a2973c03567ccfa
System Administrator <root@Adams-MacBook-Air.local>
2016-08-24 21:26:07 -0500

    Massive discovery. I don't need to import images in the component. Only include in template. Smiles all around.

--- client/KimbyArting/components/cover-page/cover-page.component.ts
+++ client/KimbyArting/components/cover-page/cover-page.component.ts
@@ -1,14 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
 
-import '../../../../public/images/cover/logo.png';
-import '../../../../public/images/cover/lightBlueBackground.png';
-import '../../../../public/images/cover/egyptGirlCover.png';
-import '../../../../public/images/cover/artstationIcon.png';
-import '../../../../public/images/cover/facebookIcon.png';
-import '../../../../public/images/cover/instagramIcon.png';
-import '../../../../public/images/cover/tumblrIcon.png';
-import '../../../../public/images/cover/twitterIcon.png';
-
 @Component({
   selector: 'cover-page',
   templateUrl: './cover-page.component.html',
