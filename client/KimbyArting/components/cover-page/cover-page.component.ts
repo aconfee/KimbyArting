083f49c2e9989d5f3b991ee9dd7080138418c84b
System Administrator <root@Adams-MacBook-Air.local>
2016-08-24 20:48:40 -0500

    dumb fucking fix because of atom typescript autogenerate

--- client/KimbyArting/components/cover-page/cover-page.component.ts
+++ client/KimbyArting/components/cover-page/cover-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 
+import '../../../../public/images/cover/logo.png';
 import '../../../../public/images/cover/lightBlueBackground.png';
 import '../../../../public/images/cover/egyptGirlCover.png';
 import '../../../../public/images/cover/artstationIcon.png';
