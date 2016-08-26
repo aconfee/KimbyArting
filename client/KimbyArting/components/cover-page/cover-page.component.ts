a1980c3fa52d12c553fd47bb5e903058072fbe2c
System Administrator <root@Adams-MacBook-Air.local>
2016-08-26 00:49:56 -0500

    lazy load social icons. not critical path

--- client/KimbyArting/components/cover-page/cover-page.component.ts
+++ client/KimbyArting/components/cover-page/cover-page.component.ts
@@ -12,17 +12,39 @@ import '../../../../public/images/cover/social.png';
 export class CoverPageComponent implements AfterViewInit{
   enter: boolean;
   exit: boolean;
+  requestSocialIcons: boolean;
   showSocialLinks: boolean;
 
   constructor(){
     this.enter = false;
     this.exit = false;
+    this.requestSocialIcons = false;
     this.showSocialLinks = false;
   }
 
   ngAfterViewInit() {
+
+    // Allow Egypt girl a tiny amount of time to load behind the scenes before animating.
     setTimeout(() => {
       this.enter = true;
     }, 200);
+  }
+
+  public ToggleSocialLinks(): void{
+
+    // Icons display is set to none. We lazy load it icons.png on first click.
+    if(this.requestSocialIcons === false){
+      this.requestSocialIcons = true;
+      console.log("remove display none");
+
+      setTimeout(() => {
+        this.showSocialLinks = true;
+        console.log("begin transition");
+      }, 20);
+
+      return;
+    }
+
+    this.showSocialLinks = !this.showSocialLinks;
   }
 }
