0c2ced49c578aa5bd954881912a1c82b990a576b
System Administrator <root@Adams-MacBook-Air.local>
2016-08-26 15:25:06 -0500

    Repeating images from data in component -- not http

--- client/KimbyArting/components/portfolio-page/portfolio-page.component.ts
+++ client/KimbyArting/components/portfolio-page/portfolio-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 
+
 @Component({
   selector: 'portfolio-page',
   templateUrl: './portfolio-page.component.html',
@@ -8,10 +9,23 @@ import { Component, AfterViewInit } from '@angular/core';
 
 export class PortfolioPageComponent implements AfterViewInit{
 
-  constructor(){
-  }
+  domain: string = 'http://localhost:3000'
+
+  images: string[] = [
+    this.domain + '/assets/egyptGirlCover.png',
+    this.domain + '/assets/egyptGirlCover.png',
+    this.domain + '/assets/egyptGirlCover.png',
+    this.domain + '/assets/egyptGirlCover.png',
+    this.domain + '/assets/egyptGirlCover.png',
+    this.domain + '/assets/egyptGirlCover.png'
+  ];
+
+
+  constructor() { }
 
   ngAfterViewInit() {
 
   }
+
+
 }
