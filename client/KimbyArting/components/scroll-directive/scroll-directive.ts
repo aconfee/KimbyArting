5335661e25020469dbd18ed997c035f2b9e35330
System Administrator <root@Adams-MacBook-Air.local>
2016-09-02 00:12:44 -0500

    Mouse scroll wheel working beautiful. Trackpad and touch still work perfectly as well. New content added.

--- client/KimbyArting/components/scroll-directive/scroll-directive.ts
+++ client/KimbyArting/components/scroll-directive/scroll-directive.ts
@@ -6,13 +6,46 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 
 export class ScrollToDirective {
 
+  trackpadEventTime: number = 0;
+  wheelEventTime: number = 0;
+  skipTrackpadUpdate: boolean = false;
+
   constructor(private el: ElementRef) {}
 
-  // Keep our scroll value up to date. Sent to parent.
-  //@HostListener('scroll')
-  //onScrollWheel() {
+  @HostListener('scroll')
+  onScroll() {
+    if(this.skipTrackpadUpdate === false){
+      this.trackpadEventTime = Date.now();
+    }
+    else{
+      this.skipTrackpadUpdate = false;
+    }
+  }
 
-  //}
+  @HostListener('wheel', ['$event.deltaY'])
+  onScrollWheel(scrollDelta) {
+    this.wheelEventTime = Date.now();
+
+    // Could be accidental noise from trackpad.
+    if(Math.abs(scrollDelta) <= 2){
+      return;
+    }
+
+    // If the wheel was used exclusively (no trackpad event for a while).
+    // This is because trackpad triggers both events, wheel only triggers one.
+    if(Math.abs(this.wheelEventTime - this.trackpadEventTime) > 500){
+
+      console.log('delta ' + scrollDelta);
+      this.setScrollX(
+        Math.min(
+          this.el.nativeElement.scrollLeft + (scrollDelta / 2),
+          this.el.nativeElement.scrollWidth)
+      );
+
+      // One time skip trackpad update since setting scrollX will trigger it.
+      this.skipTrackpadUpdate = true;
+    }
+  }
 
   setScrollX(scrollValue: number){
     this.el.nativeElement.scrollLeft = scrollValue;
